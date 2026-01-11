a1a35fad13cf1eeb4cc02394d7497749c820ab21
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-01-11 23:10:12 +0400

    .

--- _printf.c
+++ _printf.c
@@ -23,8 +23,11 @@ int _printf(const char *format, ...)
 
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+				break;
+
 			i++;
 			if (format[i] == 'c')
 				count += print_char(ap);
